2d7179a202bf571b04cbbd4953d468022dcb16a3
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-07-18 00:30:00 +0530

    Rusty

--- Snake-AI/target/debug/deps/snake_tui-4e1d90fa77a0fbd6.d
+++ Snake-AI/target/debug/deps/snake_tui-4e1d90fa77a0fbd6.d
@@ -1,7 +1,8 @@
-/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/libsnake_tui-4e1d90fa77a0fbd6.rmeta: src/lib.rs src/sim.rs src/viz.rs
+/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/libsnake_tui-4e1d90fa77a0fbd6.rmeta: src/lib.rs src/game.rs src/sim.rs src/viz.rs
 
-/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/snake_tui-4e1d90fa77a0fbd6.d: src/lib.rs src/sim.rs src/viz.rs
+/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/snake_tui-4e1d90fa77a0fbd6.d: src/lib.rs src/game.rs src/sim.rs src/viz.rs
 
 src/lib.rs:
+src/game.rs:
 src/sim.rs:
 src/viz.rs:
--- Snake-AI/target/debug/deps/snake_tui-eb68121bab72599e.d
+++ Snake-AI/target/debug/deps/snake_tui-eb68121bab72599e.d
@@ -1,7 +1,8 @@
-/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/libsnake_tui-eb68121bab72599e.rmeta: src/lib.rs src/sim.rs src/viz.rs
+/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/libsnake_tui-eb68121bab72599e.rmeta: src/lib.rs src/game.rs src/sim.rs src/viz.rs
 
-/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/snake_tui-eb68121bab72599e.d: src/lib.rs src/sim.rs src/viz.rs
+/Users/ranjanshahajishitole/Desktop/Github/Snake AI Game Rust/Rust-Snake-AI-/Snake-AI/target/debug/deps/snake_tui-eb68121bab72599e.d: src/lib.rs src/game.rs src/sim.rs src/viz.rs
 
 src/lib.rs:
+src/game.rs:
 src/sim.rs:
 src/viz.rs:
