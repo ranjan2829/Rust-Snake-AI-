84f51c83aca2a6aecb0fe95ba1c2dea57413bb51
ranjan2829 <ranjan.shitole3129@gmail.com>
2024-07-18 01:07:01 +0530

    Rusty

--- Snake-AI/src/main.rs
+++ Snake-AI/src/main.rs
@@ -1,25 +1,35 @@
+use std::time::Duration;
+use std::{io, time::Instant};
+
 use crossterm::event::{self, Event, KeyCode};
+
 use sim::Simulation;
 use snake_tui::*;
-use std::io;
-use std::time::Duration;
 
 fn main() -> io::Result<()> {
     rayon::ThreadPoolBuilder::new()
-        .num_threads(num_threads)
+        .num_threads(NUM_THREADS)
         .build_global()
         .unwrap();
-    let mut sim = Simulation::new();
+
+    let mut sim = Simulation::new()?;
+    let mut last_poll = Instant::now();
+
     loop {
-        if event::poll(Duration::from_nanos(1))? {
-            if let Event::Key(key) = event::read()? {
-                match key.code {
-                    KeyCode::Esc => break,
-                    _ => {}
+        if last_poll.elapsed() > Duration::from_millis(15) {
+            if event::poll(Duration::ZERO)? {
+                last_poll = Instant::now();
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Esc | KeyCode::Char('q') = key.code {
+                        break;
+                    }
                 }
             }
+            sim.draw();
         }
+
         sim.update();
     }
-    sim.terminate();
+
+    sim.stop()
 }
